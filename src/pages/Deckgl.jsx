a9e70d832a6e480ecf603931b81ae602c68a8f65
jmtorralvo <jm.torralvo@one-beyond.com>
2025-03-20 14:07:29 +0100

    feat(): Change elemet style when selectMode enabled: WIP

--- src/pages/Deckgl.jsx
+++ src/pages/Deckgl.jsx
@@ -1,6 +1,6 @@
 import { IconLayer } from "@deck.gl/layers";
 import { ControlPosition, MapControl } from "@vis.gl/react-google-maps";
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import DeckGLOverlay from "../components/deck-gl-overlay";
 import { UndoRedoControl } from "../components/undo-redo-control";
 import { ICON_IMAGES } from "../const";
@@ -15,6 +15,10 @@ const Deckgl = ({ filters }) => {
 	const items = filterItems({ items: audits, filters });
 	const [hoverInfo, setHoverInfo] = useState();
 	const [selectedItems, setSelectedItems] = useState([]);
+	const [selectMode, setSelectMode] = useState(false);
+
+	const isSelected = (item) =>
+		selectedItems.some((selectedItem) => selectedItem.id === item.id);
 
 	const selectItems = (bounds) => {
 		const newItems = items.filter(
@@ -28,34 +32,73 @@ const Deckgl = ({ filters }) => {
 		setSelectedItems(newItems);
 	};
 
+	const toggleSelectionItem = (item) => {
+		const index = selectedItems.findIndex(
+			(selectedItem) => selectedItem.id === item.id,
+		);
+		if (index === -1) {
+			setSelectedItems([...selectedItems, item]);
+		} else {
+			const newSelectedItems = [...selectedItems];
+			newSelectedItems.splice(index, 1);
+			setSelectedItems(newSelectedItems);
+		}
+	};
+
 	const drawingManager = useDrawingManager({ onSelect: selectItems });
 
-	const layers = [
-		new IconLayer({
-			id: "deckgl-icon",
-			data: items,
-			getColor: [255, 0, 255, 255],
-			sizeUnits: "common",
-			sizeMaxPixels: 40,
-			sizeMinPixels: 4,
-			getIcon: (d) => ({
-				url: ICON_IMAGES[d.category],
-				width: 40,
-				height: 40,
-				anchorY: 40,
+	useEffect(() => {
+		if (!drawingManager) return;
+		drawingManager.addListener("drawingmode_changed", () => {
+			const drawingMode = drawingManager.getDrawingMode();
+			console.log("drawing mode changed", drawingMode);
+			if (drawingMode) {
+				setSelectMode(true);
+				return;
+			}
+			setSelectMode(false);
+		});
+	}, [drawingManager]);
+
+	const layers = useMemo(() => {
+		const newlayers = [
+			new IconLayer({
+				id: "deckgl-icon",
+				data: items,
+				getColor: (d) => {
+					const alpha = selectMode ? 128 : 255;
+					return [255, 0, 255, alpha];
+				},
+				sizeUnits: "common",
+				sizeMaxPixels: 40,
+				sizeMinPixels: 4,
+				getIcon: (d) => ({
+					url: ICON_IMAGES[d.category],
+					width: 40,
+					height: 40,
+					anchorY: 40,
+				}),
+				getPosition: (d) => [
+					d.audit_start_coordinates.lng,
+					d.audit_start_coordinates.lat,
+					0,
+				],
+				pickable: true,
+				onClick: (info) => {
+					if (selectMode) {
+						toggleSelectionItem(info.object);
+					} else {
+						console.log("info", info);
+						setHoverInfo(info);
+					}
+				},
 			}),
-			getPosition: (d) => [
-				d.audit_start_coordinates.lng,
-				d.audit_start_coordinates.lat,
-				0,
-			],
-			pickable: true,
-			onClick: (info) => {
-				console.log("info", info);
-				setHoverInfo(info);
-			},
-		}),
-	];
+		];
+		return newlayers;
+	}, [selectMode]);
+
+	// const layers = useMemo(() => {
+	// 	}), []}
 
 	return (
 		<>
@@ -72,7 +115,7 @@ const Deckgl = ({ filters }) => {
 					zIndex: 100,
 				}}
 			>
-				<h2>DeckGL POC</h2>
+				<h2>DeckGL POC. SelectMode: {String(selectMode)}</h2>
 				<p>Amount of elements displayed: {audits.length}</p>
 				<p>Amount of elements selected: {selectedItems.length}</p>
 			</div>
@@ -91,7 +134,7 @@ const Deckgl = ({ filters }) => {
 					<p>{hoverInfo.object.id}</p>
 				</div>
 			)}
-			<DeckGLOverlay layers={layers} />
+			{Boolean(layers.length) && <DeckGLOverlay layers={layers} />}
 		</>
 	);
 };
